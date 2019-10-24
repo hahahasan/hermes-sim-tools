24fbecc891a6b031bdb2ae7e69edeb3e5070a00e
hahahasan <hm1234@york.ac.uk>
2019-10-24 16:53:55 +0100

    made gfile modifier better

--- gridgen/gfiles/gfile-mod.py
+++ gridgen/gfiles/gfile-mod.py
@@ -27,23 +27,89 @@ from boututils.datafile import DataFile
 #ld = LineDrawer()
 #ld.draw_line() # here you click on the plot
 
-def draw_boundary(gridFile):
-    grid_dat = DataFile(gridFile)
-    R = grid_dat['Rxy']
-    Z = grid_dat['Zxy']
-    nx = grid_dat['nx']
-    ny = grid_dat['ny']
+#def draw_boundary(gridFile):
+#    grid_dat = DataFile(gridFile)
+#    R = grid_dat['Rxy']
+#    Z = grid_dat['Zxy']
+#    nx = grid_dat['nx']
+#    ny = grid_dat['ny']
+#    
+#    fig, ax = plt.subplots()
+#    for i in range(nx):
+#        ax.plot(R[:, i], Z[:, i], linewidth=1, color='k', alpha=0.5)
+#        
+#    for i in range(ny):
+#        ax.plot(R[i, :], Z[i, :], linewidth=1, color='k', alpha=0.1)
+#    
+#    print('left click to select points')
+#    print('right click to deselect last point')
+#    print('middle click to finish input')
+#    plt.axis('scaled')
+#    plt.tight_layout()
+#        
+#    # global points
+#    points = plt.ginput(n=-1)
+#    points = [item for t in points for item in t]
+#    
+#    plt.close()
+#    
+#    return points
+
+
+#def extract_gpoints(gfile):
+#    f = open(gfile)
+#    lines = f.read()
+#    
+#    core = lines.split('\n\n')[0]
+#    core = core.split('\n')[1:]
+#    
+#    dec_idx = []
+#    for i in range(len(core)):
+#        dec_idx.append([pos for pos, char in enumerate(core[0]) if char == '.'])
+#    
+#    test = []
+#    for i in range(len(core)):
+#        for j in dec_idx[i]:
+#            test.append(eval(core[i][(j-2):(j+15)]))
+#        
+#    return test
+
+
+def draw_boundary(gfile):
+    f = open(gfile)
+    lines = f.read()
     
-    fig, ax = plt.subplots()
-    for i in range(nx):
-        ax.plot(R[:, i], Z[:, i], linewidth=1, color='k', alpha=0.5)
-        
-    for i in range(ny):
-        ax.plot(R[i, :], Z[i, :], linewidth=1, color='k', alpha=0.1)
+    bndry = lines.split('\n\n')[1]
+    bndry_head = bndry.split('\n')[0]
     
+    num_sep_points = eval(bndry_head.split('\t')[0])
+    num_bndry_points = eval(bndry_head.split('\t')[1].split('\n')[0])
+    
+    nsp = num_sep_points
+    nbp = num_bndry_points
+    
+    bndry = bndry.split('\n')[1:]
+    bndry = bndry[0:-1]
+    
+    dec_idx = []
+    for i in range(len(bndry)):
+        dec_idx.append([pos for pos, char in enumerate(bndry[i]) if char == '.'])
+    
+    bndry_coords = []
+    for i in range(len(bndry)):
+        for j in dec_idx[i]:
+            bndry_coords.append(eval(bndry[i][(j-2):(j+15)]))
+            
     print('left click to select points')
     print('right click to deselect last point')
     print('middle click to finish input')
+            
+    fig, ax = plt.subplots(figsize=(10,15))
+    # plt.figure(figsize=(10,15))
+    ax.scatter(bndry_coords[0:][::2][:nsp], bndry_coords[1:][::2][:nsp])
+    ax.plot(bndry_coords[0:][::2][nsp:], bndry_coords[1:][::2][nsp:])
+    # plt.gca().set_aspect('equal', adjustable='box')
+    
     plt.axis('scaled')
     plt.tight_layout()
         
@@ -52,7 +118,7 @@ def draw_boundary(gridFile):
     points = [item for t in points for item in t]
     
     plt.close()
-    
+        
     return points
     
 
@@ -98,12 +164,10 @@ a = [ 6.3400000000E-01, 7.0400000000E-01, 6.7600000000E-01, 7.5000000000E-01,
      6.7600000000E-01, -7.5000000000E-01, 6.3400000000E-01, -7.0400000000E-01,
      6.3400000000E-01, 7.0400000000E-01]
 
-points = draw_boundary('tcv_63127_64x64.nc')
-new_gfile = modify_boundary('63127_1400ms.g', bndry_coords=points)
+gfile = '63127_1400ms.g'
+
+points = draw_boundary(gfile)
+new_gfile = modify_boundary(gfile, bndry_coords=points)
 
 
 
-#plt.figure(figsize=(10,15))
-#plt.scatter(a[0:][::2], a[1:][::2])
-#plt.plot(a[0:][::2], a[1:][::2])
-#plt.gca().set_aspect('equal', adjustable='box')
